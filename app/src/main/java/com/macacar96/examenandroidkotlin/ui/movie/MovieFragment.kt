4106d1f160aa49f4051747201f4daaea4b2f4e30
Marco Carmen <58535693+MacaCar96@users.noreply.github.com>
2022-05-26 00:30:05 -0500

    [UPDATE] Se implemento el listado de las películas

--- app/src/main/java/com/macacar96/examenandroidkotlin/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/macacar96/examenandroidkotlin/ui/movie/MovieFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.macacar96.examenandroidkotlin.databinding.FragmentMovieBinding
 import com.macacar96.examenandroidkotlin.retrofit.response.MovieResponse
 
 class MovieFragment : Fragment(), MovieMVP.View {
 
-    private var _binding: FragmentMovieBinding? = null
-
+    private lateinit var _binding: FragmentMovieBinding
     private lateinit var presenter: MovieMVP.Presenter
+    private lateinit var adapter: MovieAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,23 +30,27 @@ class MovieFragment : Fragment(), MovieMVP.View {
 
         _binding = FragmentMovieBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val textView: TextView = binding.textHome
 
         presenter = MoviePresenter(this) // Creamos la instancia del presenter
         presenter.getDataPeliculasPresenter(root.context) // Inicializamos el metodo que nos traira la Data de la API Movie
 
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+        return root
     }
 
     // Método que trae el resultado de la respuesta
     override fun showResultView(result: List<MovieResponse?>?, size: Int) {
         activity?.runOnUiThread(Runnable {
-            Toast.makeText(activity, "Datos cargado con éxito: $size", Toast.LENGTH_LONG).show()
+            //Toast.makeText(activity, "Datos cargado con éxito: $size", Toast.LENGTH_LONG).show()
+
+            // Ocultamos el progress y mostramos el RV
+            _binding.pbLoading.visibility = View.GONE
+            _binding.rvMovies.visibility = View.VISIBLE
+
+            adapter = MovieAdapter(result as List<MovieResponse>, _binding!!.root.context)
+            _binding.rvMovies.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+            _binding.rvMovies.adapter = adapter
         })
     }
 
